4e159324f63636d5531ab9fef1e9cb26d13f394a
Sanya Mahajan <sm2588@cornell.edu>
2025-10-17 17:54:58 -0400

    Initial commit: profile card starter

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>React Profile Card 🪪</h1>
+      <p>Edit <code>ProfileCard.jsx</code> to create your own profile!</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/ProfileCard.jsx
@@ -0,0 +1,9 @@
+export default function ProfileCard() {
+  return (
+    <div className="profile-card">
+      <h2>Your Name</h2>
+      <p>Major: ???</p>
+      <p>Hobby: ???</p>
+    </div>
+  );
+}
